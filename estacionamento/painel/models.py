9e1b4398c706e5f40275aad1f9fefe0e54e0af05
DanielCurti <danielcurti179@gmail.com>
2019-07-27 21:16:26 -0300

    base do models

--- estacionamento/painel/models.py
+++ estacionamento/painel/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+	login = models.CharField(max_length=200)
+	senha = models.CharField(min_length=8 ,max_length=30)
+	nome  = models.Charfield(max_length=100)
+	sobrenome = models.CharField(max_length=100)
+	foto = models.ImageField()
+	telefone = models.CharField(max_length=14)
+	email = models.CharField(max_length=200)
+	nascimento = models.CharField(length=10)
+	recebimento = models.ForeingKey(Receber, on_delete = models.CASCADE)
+	vaga = models.ForeingKey(Vaga, on_delete = models.CASCADE)
+
+
+class Receber(models.Model):
+	FORMA_RECEBIMENTO = (
+		('PIC', 'PicPay'),
+		('PAY', 'PayPal'),
+	)
+	forma = models.CharField(max_length=3, choices=FORMA_RECEBIMENTO)
+	link = models.CharField(max_length=500)
+	qrcode = models.ImageField()
+
+
+class Vaga(models.model):
+	ESTADO = (
+		('RS', 'Rio Grande Do Sul'),
+		('SC', 'Santa Catarina'),
+		('PR', 'Parana'),
+	)
+	CATEGORIA (
+		('Cr', 'Carro'),
+		('Mo', 'Motocicleta'),
+		('Cn', 'Caaminhão'),
+	)
+	estado = models.CharField(max_length=2, choices=ESTADO)
+	cidade = models.CharField(max_length=100)
+	bairro = models.CharField(max_length=100)
+	rua    = models.CharField(max_length=100)
+	numero = models.CharField(max_length=7)
+	valor  = models.FloatField()
+	categoria = models.CharField(max_length=2, choices=CATEGORIA)
+	
